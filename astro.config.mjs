49537beefeb2bcaeda8f83b4feeabec2f9354151
Stardust <stardust@local.com>
2026-01-03 03:39:00 -0500

    chore: add sitemap, robots.txt, and google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ketanbpandya.com',
-  integrations: [react(), mdx()]
+  integrations: [react(), mdx(), sitemap()]
 });
